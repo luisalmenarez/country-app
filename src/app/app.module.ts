40bd1564ec6952257bf2138bf517f4962844b66a
Luis Carlos Almenarez <luisalmenarez0428@gmail.com>
2024-05-17 16:11:20 -0500

    ⚙️ Add routes and configurations pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing.module';
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [HomePageComponent, AboutPageComponent],
-  imports: [CommonModule],
-  exports: [HomePageComponent, AboutPageComponent],
+  declarations: [
+    AboutPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+    ContactPageComponent,
+  ],
+  imports: [CommonModule, RouterModule],
+  exports: [
+    AboutPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+    ContactPageComponent,
+  ],
 })
 export class SharedModule {}
