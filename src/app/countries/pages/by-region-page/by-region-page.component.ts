46a964e32a074d6d87892d323df62087f50ed7fe
Luis Almenarez <luisalmenarez0428@gmail.com>
2024-06-04 10:26:37 -0500

    ⚙️ Update

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CountriesService } from './../../services/countries.service';
 import { Country } from '../../interfaces/Country';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,18 +12,21 @@ import { Country } from '../../interfaces/Country';
 export class ByRegionPageComponent {
   public countries: Country[] = [];
   public isLoading: boolean = false;
-  public regions: string[] = [
+  public regions: Region[] = [
     'Africa',
     'Americas',
     'Asia',
     'Europe',
     'Oceania',
   ];
+  public selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService) {}
 
-  searchByRegion(term: string): void {
+  searchByRegion(term: Region): void {
     this.isLoading = true;
+
+    this.selectedRegion = term;
 
     this.countriesService.searchRegion(term).subscribe((countries) => {
       this.countries = countries;
